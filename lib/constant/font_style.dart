273026a7085649fe05d5b9a2cef87e1730c81896
Abhinav <haseeb.mohammad@ltts.com>
2023-08-27 22:31:41 +0530

    font style setup done

---
+++ lib/constant/font_style.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// This class will have all the color constant which are using in the App.
+class AppFontStyle {
+  static TextStyle numberTextStyle() {
+    return const TextStyle(
+      fontFamily: 'Lato',
+      fontWeight: FontWeight.bold,
+      fontSize: 25,
+    );
+  }
+  static TextStyle headerTextStyle() {
+    return const TextStyle(
+      fontFamily: 'Lato',
+      fontWeight: FontWeight.bold,
+      fontSize: 25,
+    );
+  }
+
+  static TextStyle titleTextStyle() {
+    return const TextStyle(
+      fontFamily: 'Lato',
+      fontWeight: FontWeight.normal,
+      fontSize: 20,
+    );
+  }
+  static TextStyle errorTextStyle() {
+    return const TextStyle(
+        fontFamily: 'Lato',
+        fontWeight: FontWeight.normal,
+        fontSize: 20,
+        color: Colors.red
+    );
+  }
+  static TextStyle successTextStyle() {
+    return const TextStyle(
+        fontFamily: 'Lato',
+        fontWeight: FontWeight.normal,
+        fontSize: 20,
+        color: Colors.red
+    );
+  }
+}
